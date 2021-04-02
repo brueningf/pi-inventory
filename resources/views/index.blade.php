ef0ac3067b7076ca9ab728d08fe14b611f9766af
FredericBruening <contact@fredesk.com>
2021-04-02 14:53:37 -0500

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,16 +2,21 @@
 
 @section('content')
     <div class="p-2 sm:p-6 mt-5">
-        <div>
-
+        <div class="flex items-start justify-start flex-wrap">
+            @foreach($categories as $category)
+                <a class="w-1/2 sm:w-1/6 border border-gray-500 overflow-hidden py-2 px-3 bg-gray-600 hover:bg-gray-400 text-white hover:text-gray-200 text-left" href="{{ $category->path() }}">
+                   {{ $category->name }}
+                </a>
+            @endforeach
         </div>
-        <h3 class="text-xl font-bold mt-6">Recently updated items
-            <div class="flex items-center justify-between px-4 py-3 text-gray-300 bg-gray-700">
+        <h3 class="text-xl font-bold mt-6 flex items-center justify-between">
+            <span>Recently updated items</span>
+            <span class="flex items-center justify-between text-gray-300">
                 <span class="hidden sm:block mr-2">Backup</span>
                 <zondicon @if(config('backup_health.checked')) icon="checkmark-outline"
                           @else icon="exclamation-outline" @endif
                           class="w-5 h-5 fill-current mr-1 @if(config('backup_health.checked')) text-green-500 @else text-yellow-500 @endif"></zondicon>
-            </div>
+            </span>
         </h3>
         <div class="w-full flex flex-wrap my-6">
             @foreach ($recentEditedItems as $item)
